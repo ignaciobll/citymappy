5ef065b2d0d9d980a0be7736a457c2cbfe1f5f5a
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-06-10 22:57:50 +0200

    Initial commit. Ready to work

---
+++ citymappy/madBus.py
@@ -0,0 +1,32 @@
+import json
+import requests
+import os.path
+
+stops = {}
+
+with open("{}/stops.json".format(os.path.dirname(__file__)), 'r+') as f:
+    print("Abierto fichero con la paradas")
+    stops = json.loads(f.read())
+
+with open("{}/headers.json".format(os.path.dirname(__file__)), 'r') as h:
+    headers = json.loads(h.read())
+
+
+def get_stop_time(idStop):
+
+    url = 'https://citymapper.com/api/1/departures?headways=1&ids='
+
+    ids = stops[str(idStop)]['id']
+    r = requests.get(url + ids, headers=headers)
+    j = json.loads(r.text)
+    x = j['stops'][0]['services']
+    response = {'stops': [{}, {}]}
+    for i in range(2):
+        response['stops'][i]['name'] = x[i]['route_id'].strip('EMTBus')
+        response['stops'][i]['headsign'] = x[i]['headsign']
+        try:
+            response['stops'][i]['arrival'] = x[i]['live_departures_seconds'][0]
+        except:
+            response['stops'][i]['arrival'] = str(x[i]['next_departures'][0])
+
+    return response
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='citymappy',
+      version='0.1',
+      description='Citymapper API',
+      url='',
+      author='CityMapper Ignacio Ballesteros',
+      author_email='',
+      license='MIT',
+      packages=['citymappy'],
+      include_package_data=True,
+      package_data={'': ['*.json']},
+      zip_safe=False)
